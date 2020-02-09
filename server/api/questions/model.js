bcce050f1f80c51f1663bf6ecf3ad170bd343827
Mosaab Emam <mosaab.emam123@gmail.com>
2020-02-10 00:03:13 +0200

    design improvements; added discipline and lesson filters

--- server/api/questions/model.js
+++ server/api/questions/model.js
@@ -2,14 +2,18 @@ const mongoose = require("mongoose");
 
 const QuestionSchema = new mongoose.Schema({
   text: {
-	  type: String,
-	  required: true
+    type: String,
+    required: true
   },
   choices: {
-	  type: Array,
-	  required: true
+    type: Array,
+    required: true
   },
   descipline: {
+    type: String,
+    required: true
+  },
+  lesson: {
     type: String,
     required: true
   }
--- server/api/questions/router.js
+++ server/api/questions/router.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const Question = require("./model");
 
-router.get("", (req, res) => {
-  Question.find({}).then(data => {
+router.get("/:discipline/:lesson", (req, res) => {
+  const { discipline, lesson } = req.params;
+  Question.find({ discipline, lesson }).then(data => {
     res.send(data);
   });
 });
@@ -17,7 +18,7 @@ router.post("", (req, res) => {
   let question = new Question({
     text: req.body.text,
     choices: req.body.choices,
-    descipline: req.body.descipline
+    discipline: req.body.descipline
   });
   question.save().then(q => {
     res.send(q);
